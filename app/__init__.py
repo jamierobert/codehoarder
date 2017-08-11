4584ef42307540c5262db72fe41b14d365cbfd31
Jamie Roberts <jmaie.roberts1111@gmail.com>
2017-08-11 11:06:16 +0100

    posts now render in browser correctly

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_pagedown import PageDown
 from config import config
+from flaskext.markdown import Markdown
 
 bootstrap = Bootstrap()
 mail = Mail()
@@ -22,13 +23,15 @@ def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
-
+    ##This is where I instantiatie markdown
+    markdown = Markdown(app)
     bootstrap.init_app(app)
     mail.init_app(app)
     moment.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
     pagedown.init_app(app)
+
 
     if not app.debug and not app.testing and not app.config['SSL_DISABLE']:
         from flask_sslify import SSLify
