e1450ac41677c6516d319b360193611af0e57f2c
Jamie Roberts <jmaie.roberts1111@gmail.com>
2017-08-11 09:23:09 +0100

    change from flask.ext.wtf import Form to from flask_wtf import FlaskForm to remove warning

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,4 +1,4 @@
-from flask.ext.wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, BooleanField, SelectField,\
     SubmitField
 from wtforms.validators import Required, Length, Email, Regexp
@@ -7,19 +7,19 @@ from flask_pagedown.fields import PageDownField
 from ..models import Role, User
 
 
-class NameForm(Form):
+class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[Required()])
     submit = SubmitField('Submit')
 
 
-class EditProfileForm(Form):
+class EditProfileForm(FlaskForm):
     name = StringField('Real name', validators=[Length(0, 64)])
     location = StringField('Location', validators=[Length(0, 64)])
     about_me = TextAreaField('About me')
     submit = SubmitField('Submit')
 
 
-class EditProfileAdminForm(Form):
+class EditProfileAdminForm(FlaskForm):
     email = StringField('Email', validators=[Required(), Length(1, 64),
                                              Email()])
     username = StringField('Username', validators=[
@@ -50,7 +50,7 @@ class EditProfileAdminForm(Form):
             raise ValidationError('Username already in use.')
 
 
-class PostForm(Form):
+class PostForm(FlaskForm):
     title = PageDownField("Title", validators=[Required()])
     sub_title = PageDownField("sub_title", validators=[Required()])
     image = PageDownField("Image Path", validators=[Required()])
@@ -58,6 +58,6 @@ class PostForm(Form):
     submit = SubmitField('Submit')
 
 
-class CommentForm(Form):
+class CommentForm(FlaskForm):
     body = StringField('Enter your comment', validators=[Required()])
     submit = SubmitField('Submit')
